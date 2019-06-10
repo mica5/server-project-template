7983b116839cc95350d53288e008b8a5e6c503f4
mica <mica.eldridge1@gmail.com>
2019-06-10 14:40:48 -0700

    added a sample interaction, jquery

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import urllib
 import subprocess
 from dateutil import parser
 
+import jinja2
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
 def log(message):
@@ -18,7 +19,7 @@ from helpers import load_keys
 class HTTPSResource(object):
     """docstring for HTTPSResource"""
     reload_templates = True
-    # jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(this_dir, 'templates')))
+    jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(this_dir, 'templates')))
 
     keydict = load_keys()
 
@@ -84,14 +85,40 @@ class RootResource(HTTPSResource):
         )
 
 class IndexResource(HTTPSResource):
+    def __init__(self, *args, **kwargs):
+        super(IndexResource, self).__init__(*args, **kwargs)
+        self.template = self._load_template()
+
     def on_get(self, req, resp):
         super(IndexResource, self).on_get(req, resp)
         resp.content_type = falcon.MEDIA_HTML
-        resp.body = (
-            '''<html><head></head><body>
-            It works!
-            </body></html>\n'''
-        )
+        key = req.params.get('key', None)
+        key = 'key={}'.format(key) if key else ''
+
+        template = self.template
+        if self.reload_templates:
+            template = self._load_template()
+
+        resp.body = template.render(key=key)
+        return
+
+    @classmethod
+    def _load_template(cls):
+        with open(os.path.join(this_dir, 'templates/index.html'), 'r') as fr:
+            template = cls.jinja_env.from_string(fr.read())
+        return template
+
+
+class TestResource(HTTPSResource):
+    def __init__(self, *args, **kwargs):
+        super(TestResource, self).__init__(*args, **kwargs)
+
+    def on_post(self, req, resp):
+        super(TestResource, self).on_get(req, resp)
+        resp.content_type = falcon.MEDIA_TEXT
+        resp.body = 'test response'
+        return
+
 
 
 
@@ -99,3 +126,4 @@ api = falcon.API()
 
 api.add_route('/', RootResource())
 api.add_route('/index.html', IndexResource())
+api.add_route('/test', TestResource())
